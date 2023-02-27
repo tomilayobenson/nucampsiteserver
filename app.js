7c842b12613259fec5ea06e81a4bfac9cbfe2533
TomilayoAfolabi <tomilayoafolabi@gmail.com>
2023-02-26 21:42:17 -0700

    added promotion routers and partner routers with mongodb connection

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/nucampsite';
+const connect = mongoose.connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+connect.then(()=> console.log('Connected correctly to server'),err => console.log(err))
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const campsiteRouter = require('./routes/campsiteRouter');
+const partnerRouter = require('./routes/partnerRouter');
+const promotionRouter = require('./routes/promotionRouter');
 
 var app = express();
 
@@ -21,6 +34,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
