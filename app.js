6ce08fb018341b2d4b8f2e5565eacc8f10c2e71f
TomilayoAfolabi <tomilayoafolabi@gmail.com>
2023-03-02 14:31:38 -0700

    sessions with user authentication against mongodb

--- app.js
+++ app.js
@@ -34,38 +34,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 app.use(session({
-  name:'session-id',
+  name: 'session-id',
   secret: '12345-67890-09876-54321',
   saveUninitialized: false,
   resave: false,
   store: new FileStore()
 }))
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
+  console.log(req.session);
   if (!req.session.user) {
-    const authHeader = req.headers.authorization
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!')
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const user = auth[0]
-    const pass = auth[1]
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin'
-      next() //authorized
-    } else {
-      const err = new Error('You are not authenticated!')
-      res.setHeader('WWW-Authenticate', 'Basic')
-      err.status = 401
-      return next(err)
-    }
+    const err = new Error('You are not at all authenticated!')
+    err.status = 401
+    return next(err)
   } else {
-    if(req.session.user==='admin'){
+    if (req.session.user === 'authenticated') {
       next()
-    }else {
+    } else {
       const err = new Error('You are not authenticated!')
       err.status = 401
       return next(err)
@@ -78,8 +66,7 @@ app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
