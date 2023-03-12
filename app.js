d026f82a2368361f0c8d6fc8295303d139947183
TomilayoAfolabi <tomilayoafolabi@gmail.com>
2023-03-11 22:43:52 -0700

    added favorited route

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const uploadRouter = require('./routes/uploadRouter')
+const favoriteRouter = require('./routes/favoriteRouter')
 
 var app = express();
 
@@ -52,6 +53,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload',uploadRouter)
+app.use('/favorites',favoriteRouter)
 
 
 // catch 404 and forward to error handler
